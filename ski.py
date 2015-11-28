c774adbc73a54f42706d8e4a160c9fd241b44e93
PRuffy <pierre.ruffy@gmail.com>
2015-11-28 11:39:30 +0100

    commit class ski

---
+++ ski.py
@@ -0,0 +1,12 @@
+import networkx as nx
+
+class skiDomain(object):
+	"""docstring for skiDomain"""
+	def __init__(self):
+		numberOfEdge
+		numberOfVertice
+		domain = nx.MultiDiGraph()
+		
+	def createGraph(self, data):
+		self.numberOfEdge = int(data[0][0])
+		self.numberOfVertice = int(data[self.numberOfEdge+2][0])
