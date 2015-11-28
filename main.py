ed138c6f4461eaf19c2b7300fd7bfaacb18356ce
PRuffy <pierre.ruffy@gmail.com>
2015-11-28 11:16:25 +0100

    Code commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def getGraphData():
+	data=[]
+	
+	with open("./dataski.txt","r") as ski_file:
+		skiData=ski_file.read()	#read the file
+
+		lignes=skiData.split("\n") #parse line by line
+
+		for case in lignes:	#parse each line
+			elem=case.split("	")
+			data.append(elem)
+
+	ski_file.close()
+	
+	return(data)
+
+if __name__ == "__main__":
+
+	domainData = getGraphData()
+	print(domainData)
